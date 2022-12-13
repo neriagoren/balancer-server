6c0b3d414d223fbc351af924399c0d4d94eb5081
neriagini <neriag@gini-apps.com>
2022-12-13 18:08:35 +0200

    testing

--- index.js
+++ index.js
@@ -2,11 +2,38 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-const expenses = [{
-    id: 0,
-    category: 'Food',
-    value: 100,
-}]
+function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+}
+
+function subDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() - days);
+    return result;
+}
+
+const expenses = [
+    {
+        id: 0,
+        category: 'Food',
+        value: 100,
+        date: Date.now(),
+    },
+    {
+        id: 1,
+        category: 'Other',
+        value: 200,
+        date: subDays(Date.now(), 1)
+    },
+    {
+        id: 2,
+        category: 'Electronics',
+        value: 300,
+        date: subDays(Date.now(), 2)
+    }
+]
 
 app.get('/', (req, res) => {
     res.json(expenses)
