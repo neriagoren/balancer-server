a4d9478525d5574e44502a5366757342c750e79d
neriagoren <neriatheman@gmail.com>
2022-12-29 13:33:42 +0200

    g

--- index.js
+++ index.js
@@ -8,17 +8,10 @@ dotenv.config();
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization, 'Content-Type' : 'multipart/form-data' ,* "
-    );
-    res.header(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
+    app.options('*', cors())
     next();
-});
-
+}
+);
 app.use(cors())
 
 app.get('/get-all-expenses', (req, res, next) => {
