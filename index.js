f82462f18bfd344f9151511e68d66f209f8efd08
neriagoren <neriatheman@gmail.com>
2022-12-29 13:55:40 +0200

    serving static files

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 const cors = require('cors');
 const port = 8080
 const db = require('./db');
 const dotenv = require('dotenv');
 dotenv.config();
 
-app.use(cors({
-    origin: 'https://original-balancer.netlify.app'
-}));
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 app.get('/get-all-expenses', (req, res, next) => {
     db.pool.getConnection(async function (err, conn) {
