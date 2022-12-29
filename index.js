eab22a84bfa28438c1ce4b7477557e5f7a5d3ddb
neriagoren <neriatheman@gmail.com>
2022-12-29 12:32:56 +0200

    added route

--- index.js
+++ index.js
@@ -26,6 +26,25 @@ app.get('/get-all-expenses', (req, res, next) => {
     });
 })
 
+app.post('/add-expense', (req, res, next) => {
+    db.pool.getConnection(async function (err, conn) {
+        if (err) {
+            res.status(500).send({ message: 'something went wrong!' })
+        } else {
+            conn.query('INSERT INTO expenses (category, value, date) VALUES (?,?,now());',
+                [req.body.category, req.body.value], (err, data) => {
+                    if (err) {
+                        res.status(500).send({ message: 'something went wrong!' })
+                    } else {
+                        res.status(200).send({ message: 'inserted successfuly!' })
+                    }
+                })
+            conn.release();
+        }
+    });
+})
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
